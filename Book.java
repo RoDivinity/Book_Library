b4414f5e0b5ada0ee957f2c77af78248ae08f13a
Linh Nguyen <h.linh.nguyen@tcu.edu>
2016-06-29 15:55:15 -0500

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,36 @@
+/**  
+ *  Book class container includes:
+ * Book title
+ * Author name
+ * Book Genre
+ * => Information related to progression (number of pages/chapter, 
+ *                pages/chapter finished, file directory to the book(if stored on computer))
+ * => Summary  (time taken to read, number of times read, summary of book, comments)
+ */
+package Bk_Lib_Model;
+
+/**
+ * @Version 1.0.0:
+ *      +Book title, author name and book genre added
+ *      +Book progression (number of pages/chapter)
+ *      +Summary (comments , book summary)
+ *      +Merge everything to book data, so this book class is for linked list
+ *  <==FUTURE UPDATE ==>
+ *      +Completed file directory. Open book when click
+ *      +Track down what day you start reading, what day you complete
+ *      +Allow to write comments, book summary
+ *      +Open file in separate reader, allowing user to directly comments, bookmarks, etc...
+ * @author Vodka
+ */
+public class Book {
+   
+   
+public Book ()
+{
+    
+}
+
+
+
+
+}
